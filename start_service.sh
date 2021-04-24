fcc7f292385b35d572e7be14006f8bccc44f773e
Oscar Lundberg <info@oscarlundberg.com>
2021-04-24 13:24:49 +0200

    updated startscript

--- start_service.sh
+++ start_service.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+EX_PATH="`dirname \"$0\"`"              # relative
+EX_PATH="`( cd \"$EX_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$EX_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+cd "$EX_PATH";
 npm install;
 npm run start;
